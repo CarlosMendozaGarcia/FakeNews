255d27f806d110b01fdb2aa8851804c83a69e9fa
Orion <mendozagarcia@uninorte.edu.co>
2022-04-27 15:37:58 -0500

    print fake news base on a list of News

--- Main.py
+++ Main.py
@@ -1,5 +1,31 @@
+import nltk
 from Scrapping import Titulares
+from csv import writer
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize
 
-url='https://www.eltiempo.com/mundo'
-Tiempo= Titulares(url)
-Tiempo.printTitulares()
+
+url='https://www.bbc.com/mundo/topics/c2lej05epw5t'
+BBC= Titulares(url)
+titulosejemplo=BBC.getTitulares()
+
+#escribe los titulares en el csv
+print('cambio')
+with open('FakeNews.csv','w',newline='',encoding='utf-8') as f:
+    for titulo in titulosejemplo:
+        f.write(titulo+'\n')
+
+
+listword=[]
+for i in range(0,len(titulosejemplo)):
+    aux= word_tokenize(titulosejemplo[i])
+    listword.append(aux)
+
+stop_words= set(stopwords.words('spanish'))
+filteredlist=[]
+
+for i in range(0,len(listword)):
+    for word in listword[i]:
+        if word.casefold() not in stop_words:
+            filteredlist.append(word)
+Nouns=[]
--- Scrapping.py
+++ Scrapping.py
@@ -1,3 +1,4 @@
+import string
 import urllib, bs4
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
@@ -6,16 +7,19 @@ class Titulares():
     titulares=[]
     def __init__(self,url):
         self.url= url
-        http= urlopen(url).read()
-        self.html= BeautifulSoup(http,features='html.parser')
         self.setTitulares()
     def setTitulares(self):
-        i=0
-        for titulos in self.html.find_all('a',class_="title page-link"):
-            fechahora= self.html.find_all('span',class_="published-at")[i].text.strip()
-            titulos=self.html.find_all('a',class_="title page-link")[i].text.strip()
-            self.titulares.append(fechahora+';'+titulos)
-            i +=1
+        for i in range(1,100):
+            http= urlopen(self.url+'/page/'+str(i)).read()
+            html= BeautifulSoup(http,features='html.parser')
+            j=0
+            for titulos in html.find_all('span',class_="lx-stream-post__header-text gs-u-align-middle"):
+                fechahora= html.find_all('span',class_="qa-post-auto-meta")[j].text.strip()
+                titulos=html.find_all('span',class_="lx-stream-post__header-text gs-u-align-middle")[j].text.strip()
+                self.titulares.append(fechahora+';'+titulos)
+                j +=1
     def printTitulares(self):
         for i in range(0,len(self.titulares)):
             print(f'titular[{i}]= {self.titulares[i]}')
+    def getTitulares(self):
+            return self.titulares
